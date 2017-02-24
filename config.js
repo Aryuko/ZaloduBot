6ce4883ce8be20e55a7af0d135b3d1d59b30df36
Zalodu <peter.kjellen@gmail.com>
2017-02-25 00:32:05 +0100

    Refactor to .js to allow comments

---
+++ config.js
@@ -0,0 +1,16 @@
+// Use "git update-index --assume-unchanged .\config.js" if you're going to make commits in this repo.
+// Otherwise you risk commiting sensitive information.
+// Use "git update-index --no-assume-unchanged .\config.js" to be able to commit changes to this file.
+module.exports = {
+    "authentication": {
+        "bot_token": "insert token here",
+        "client_id": "insert client id here"
+    },
+    "embeds": {
+        "defaultColor": "#738BD7",
+        "errorColor": "#f74f25"
+    },
+    "user": {
+        "status": "invisible"
+    }
+};
